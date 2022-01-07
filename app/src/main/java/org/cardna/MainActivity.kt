c02c3bbc321a997107a71dd51fc0f936a73692f1
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-01-08 02:12:33 +0900

    [CHORE] #1 - DataBinding으로 변경, MainActivity BaseViewUtil

--- app/src/main/java/org/cardna/MainActivity.kt
+++ app/src/main/java/org/cardna/MainActivity.kt
@@ -2,10 +2,11 @@ package org.cardna
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.cardna.base.baseutil.BaseViewUtil
+import org.cardna.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseViewUtil.BaseAppCompatActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
