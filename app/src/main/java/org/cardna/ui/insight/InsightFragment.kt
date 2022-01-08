573ccc108e56aba065b09ef8e85a4a793b6b56f5
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-01-09 00:51:48 +0900

    [ADD] #5 - Add BottomNavigation Fragment

---
+++ app/src/main/java/org/cardna/ui/insight/InsightFragment.kt
@@ -0,0 +1,18 @@
+package org.cardna.ui.insight
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.cardna.R
+import org.cardna.base.baseutil.BaseViewUtil
+import org.cardna.databinding.FragmentCardPackBinding
+import org.cardna.databinding.FragmentInsightBinding
+
+class InsightFragment : BaseViewUtil.BaseFragment<FragmentInsightBinding>(R.layout.fragment_insight) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/org/cardna/ui/maincard/MainCardFragment.kt
@@ -0,0 +1,18 @@
+package org.cardna.ui.maincard
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.cardna.R
+import org.cardna.base.baseutil.BaseViewUtil
+import org.cardna.databinding.FragmentInsightBinding
+import org.cardna.databinding.FragmentMainCardBinding
+
+class MainCardFragment : BaseViewUtil.BaseFragment<FragmentMainCardBinding>(R.layout.fragment_main_card) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
