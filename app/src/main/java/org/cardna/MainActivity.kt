ee1e65429a02f87a51ed3efc892a9ef08d3cc588
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-01-09 13:12:12 +0900

    [ADD] #5 - Connect Bottomnavigation

--- app/src/main/java/org/cardna/MainActivity.kt
+++ app/src/main/java/org/cardna/MainActivity.kt
@@ -1,12 +1,24 @@
 package org.cardna
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import org.cardna.base.baseutil.BaseViewUtil
 import org.cardna.databinding.ActivityMainBinding
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : BaseViewUtil.BaseAppCompatActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setBottomNavigation()
+    }
+
+    private fun setBottomNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fcv_main) as NavHostFragment
+        val navController = navHostFragment.findNavController()
+        binding.bnvMain.setupWithNavController(navController)
+        binding.bnvMain.isItemHorizontalTranslationEnabled = true
     }
 }
