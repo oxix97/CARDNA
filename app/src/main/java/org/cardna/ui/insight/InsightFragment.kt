edf9be4a40359f7737abd8a79df0f38429f55d1c
username <ieejo716@naver.com>
2022-01-12 23:18:46 +0900

    [FEAT]: RepresentCardEditActivity

--- app/src/main/java/org/cardna/ui/insight/InsightFragment.kt
+++ app/src/main/java/org/cardna/ui/insight/InsightFragment.kt
@@ -6,9 +6,9 @@ import org.cardna.R
 import org.cardna.base.baseutil.BaseViewUtil
 import org.cardna.databinding.FragmentInsightBinding
 
-class InsightFragment : BaseViewUtil.BaseFragment<FragmentInsightBinding>(R.layout.fragment_insight) {
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-    }
-}
+// class InsightFragment : BaseViewUtil.BaseFragment<FragmentInsightBinding>(R.layout.fragment_insight) {
+//
+//     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//         super.onViewCreated(view, savedInstanceState)
+//     }
+// }
