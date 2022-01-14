0fd186dda8d9f384296a36eaaa29e54fe4583426
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-01-14 13:42:49 +0900

    [ADD] #18 - searchNameButton Keyboard done event

--- app/src/main/java/org/cardna/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/org/cardna/ui/mypage/MyPageFragment.kt
@@ -1,7 +1,9 @@
 package org.cardna.ui.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import androidx.recyclerview.widget.GridLayoutManager
 import org.cardna.base.baseutil.BaseViewUtil
 import org.cardna.data.remote.model.mypage.ResponseMyPageFriendData
@@ -9,6 +11,7 @@ import org.cardna.databinding.FragmentMyPageBinding
 import org.cardna.ui.maincard.MainCardFragment
 import org.cardna.ui.mypage.adapter.MyPageFriendAdapter
 import org.cardna.util.SpacesItemDecoration
+import org.cardna.util.shortToast
 
 
 class MyPageFragment : BaseViewUtil.BaseFragment<FragmentMyPageBinding>(org.cardna.R.layout.fragment_my_page) {
@@ -21,6 +24,7 @@ class MyPageFragment : BaseViewUtil.BaseFragment<FragmentMyPageBinding>(org.card
     override fun initView() {
         initScrollView()
         myPageRecyclerViewAdapter()
+        initClickEvent()
     }
 
     private fun initScrollView() {
@@ -60,5 +64,30 @@ class MyPageFragment : BaseViewUtil.BaseFragment<FragmentMyPageBinding>(org.card
 
         binding.rvMypage.addItemDecoration(SpacesItemDecoration(12))
         myPageFriendAdapter.notifyDataSetChanged()
+    }
+
+    private fun initClickEvent() {
+        with(binding) {
+
+            etMypageSearchBackground.setOnEditorActionListener { textView, action, event ->
+                var handled = false
+                if (action == EditorInfo.IME_ACTION_DONE) {
+                    //검색완료 키보드 이벤트 완료 시 리사이클러뷰 item 새로 띄우기
+                    requireContext().shortToast("test")
+                    handled = true
+                }
+                handled
+            }
+
+            ivMypageGotoEmailsearch.setOnClickListener {
+                startActivity(Intent(requireContext(), SearchEmailActivity::class.java))
+            }
+            ivMypageOtherWrote.setOnClickListener {
+                startActivity(Intent(requireContext(), OtherWriteActivity::class.java))
+            }
+            ivMypageSetting.setOnClickListener {
+                startActivity(Intent(requireContext(), SettingActivity::class.java))
+            }
+        }
     }
 }
---
+++ app/src/main/java/org/cardna/util/ViewExt.kt
@@ -0,0 +1,9 @@
+package org.cardna.util
+
+import android.content.Context
+import android.widget.Toast
+
+
+    fun Context.shortToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
