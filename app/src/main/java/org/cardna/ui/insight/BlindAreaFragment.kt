314078d247c62ca7c1f0ae05e2d0de76085d418d
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-01-15 07:07:55 +0900

    [ADD] #27 - Add BuildAreaFragment & OpenAreaFragment

---
+++ app/src/main/java/org/cardna/ui/insight/BlindAreaFragment.kt
@@ -0,0 +1,13 @@
+package org.cardna.ui.insight
+
+import org.cardna.R
+import org.cardna.base.baseutil.BaseViewUtil
+import org.cardna.databinding.FragmentBlindAreaBinding
+import org.cardna.databinding.FragmentOpenAreaBinding
+
+class BuildAreaFragment : BaseViewUtil.BaseFragment<FragmentBlindAreaBinding>(R.layout.fragment_blind_area) {
+
+    override fun initView() {
+
+    }
+}
