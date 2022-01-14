94eba60afb7e20c3cfbc0c35557f4a270f119c46
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-01-14 12:47:45 +0900

    [FEAT] #18 - MypageFragment to MainCardFragment

--- app/src/main/java/org/cardna/ui/mypage/MyPageFragment.kt
+++ app/src/main/java/org/cardna/ui/mypage/MyPageFragment.kt
@@ -3,7 +3,6 @@ package org.cardna.ui.mypage
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
-import org.cardna.R
 import org.cardna.base.baseutil.BaseViewUtil
 import org.cardna.data.remote.model.mypage.ResponseMyPageFriendData
 import org.cardna.databinding.FragmentMyPageBinding
@@ -11,7 +10,8 @@ import org.cardna.ui.maincard.MainCardFragment
 import org.cardna.ui.mypage.adapter.MyPageFriendAdapter
 import org.cardna.util.SpacesItemDecoration
 
-class MyPageFragment : BaseViewUtil.BaseFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+
+class MyPageFragment : BaseViewUtil.BaseFragment<FragmentMyPageBinding>(org.cardna.R.layout.fragment_my_page) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -32,20 +32,28 @@ class MyPageFragment : BaseViewUtil.BaseFragment<FragmentMyPageBinding>(R.layout
     private fun myPageRecyclerViewAdapter() {
         val myPageFriendAdapter = MyPageFriendAdapter(
             listOf(
-                ResponseMyPageFriendData(1, "다빈", R.drawable.img_friend_image, "하이"),
-                ResponseMyPageFriendData(1, "라빈", R.drawable.img_friend_image, "하이"),
-                ResponseMyPageFriendData(1, "마빈", R.drawable.img_friend_image, "하이"),
-                ResponseMyPageFriendData(1, "바빈", R.drawable.img_friend_image, "하이"),
-                ResponseMyPageFriendData(1, "사빈", R.drawable.img_friend_image, "하이"),
-                ResponseMyPageFriendData(1, "아빈", R.drawable.img_friend_image, "하이")
+                ResponseMyPageFriendData(1, "다빈", org.cardna.R.drawable.img_friend_image, "하이"),
+                ResponseMyPageFriendData(1, "라빈", org.cardna.R.drawable.img_friend_image, "하이"),
+                ResponseMyPageFriendData(1, "마빈", org.cardna.R.drawable.img_friend_image, "하이"),
+                ResponseMyPageFriendData(1, "바빈", org.cardna.R.drawable.img_friend_image, "하이"),
+                ResponseMyPageFriendData(1, "사빈", org.cardna.R.drawable.img_friend_image, "하이"),
+                ResponseMyPageFriendData(1, "아빈", org.cardna.R.drawable.img_friend_image, "하이")
             )
-        ) { position ->
+        ) { item ->
+            val bundle = Bundle()
+            bundle.putInt("userImg", item.userImg)
+            bundle.putString("name", item.name)
+            bundle.putString("sentence", item.sentence)
+
+            val mainCardFragment = MainCardFragment()
+            mainCardFragment.setArguments(bundle)
+
             val transaction = requireActivity().supportFragmentManager.beginTransaction()
                 .addToBackStack(null)
-                .replace(R.id.fcv_main, MainCardFragment())
+                .replace(org.cardna.R.id.fcv_main, mainCardFragment)
             transaction.commit()
         }
-
+        
         binding.rvMypage.adapter = myPageFriendAdapter
 
         val gridLayoutManager = GridLayoutManager(requireContext(), 2)
--- app/src/main/java/org/cardna/ui/mypage/adapter/MyPageFriendAdapter.kt
+++ app/src/main/java/org/cardna/ui/mypage/adapter/MyPageFriendAdapter.kt
@@ -8,16 +8,18 @@ import org.cardna.databinding.ItemMypageFriendBinding
 
 class MyPageFriendAdapter(
     private val friendList: List<ResponseMyPageFriendData>,
-    private val clickListener: (Int) -> Unit
+    private val clickListener: (ResponseMyPageFriendData) -> Unit
 ) : RecyclerView.Adapter<MyPageFriendAdapter.MyPageFriendViewHolder>() {
 
     inner class MyPageFriendViewHolder(private val binding: ItemMypageFriendBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun onBind(data: ResponseMyPageFriendData, clickListener: (Int) -> Unit, position: Int) {
-            binding.ivFriendImage.setImageResource(data.userImg)
-            binding.tvFriendName.text = data.name
-            binding.tvFriendSentence.text = data.sentence
-            binding.root.setOnClickListener {
-                clickListener(position)
+        fun onBind(data: ResponseMyPageFriendData) {
+            binding.apply {
+                ivFriendImage.setImageResource(data.userImg)
+                tvFriendName.text = data.name
+                tvFriendSentence.text = data.sentence
+                root.setOnClickListener {
+                    clickListener(data)
+                }
             }
         }
     }
@@ -28,7 +30,7 @@ class MyPageFriendAdapter(
     }
 
     override fun onBindViewHolder(holder: MyPageFriendAdapter.MyPageFriendViewHolder, position: Int) {
-        return holder.onBind(friendList[position], clickListener, position)
+        return holder.onBind(friendList[position])
 
     }
 
