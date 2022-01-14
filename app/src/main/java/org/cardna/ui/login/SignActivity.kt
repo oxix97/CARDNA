a07260e21f76d126813b06a6bd9cbe1129d13aed
username <ieejo716@naver.com>
2022-01-15 03:35:58 +0900

    # 26 "[FEAT] : SignActivity"

--- app/src/main/java/org/cardna/ui/login/SignActivity.kt
+++ app/src/main/java/org/cardna/ui/login/SignActivity.kt
@@ -1,10 +1,7 @@
 package org.cardna.ui.login
 
-import android.graphics.Color
-import android.graphics.LinearGradient
-import android.graphics.Shader
+import android.content.Intent
 import android.os.Bundle
-import com.bumptech.glide.Glide
 import org.cardna.R
 import org.cardna.base.baseutil.BaseViewUtil
 import org.cardna.databinding.ActivitySignBinding
@@ -13,19 +10,24 @@ class SignActivity :
     BaseViewUtil.BaseAppCompatActivity<ActivitySignBinding>(R.layout.activity_sign) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Glide
-            .with(this)
-            .load(R.drawable.ic_logo)
-            .into(binding.ivSignLogo)
-
-        // val shader: Shader = LinearGradient(
-        //     0f, 0f, 20f, 20f, listOf<Long>(
-        //         R.color.main_green.toLong(),
-        //         R.color.main_purple.toLong(),
-        //     ), 20f, 0.1, Shader.TileMode.CLAMP
-        // )
+        signUpClick()
+        signInClick()
     }
 
     override fun initView() {
+    }
+
+    private fun signInClick() {
+        binding.btnSignIn.setOnClickListener {
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun signUpClick() {
+        binding.btnSignUp.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
