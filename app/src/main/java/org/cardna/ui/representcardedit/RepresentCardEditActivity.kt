743daf397ec730f1a328a2f7b8655305d12d392b
username <ieejo716@naver.com>
2022-01-16 02:03:38 +0900

    # 15 "[REFACTOR] : RepresentActivity gradient text apply

--- app/src/main/java/org/cardna/ui/representcardedit/RepresentCardEditActivity.kt
+++ app/src/main/java/org/cardna/ui/representcardedit/RepresentCardEditActivity.kt
@@ -1,12 +1,15 @@
 package org.cardna.ui.representcardedit
 
 import android.os.Bundle
+import androidx.core.text.set
+import androidx.core.text.toSpannable
 import androidx.recyclerview.widget.GridLayoutManager
 import org.cardna.R
 import org.cardna.base.baseutil.BaseViewUtil
 import org.cardna.data.remote.api.RepresentCardData
 import org.cardna.databinding.ActivityRepresentCardEditBinding
 import org.cardna.ui.maincard.adapter.RepresentCardListAdapter
+import org.cardna.util.LinearGradientSpan
 import org.cardna.util.SpacesItemDecoration
 
 class RepresentCardEditActivity :
@@ -16,6 +19,7 @@ class RepresentCardEditActivity :
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
+        setTextGradient()
     }
 
     override fun initView() {
@@ -83,5 +87,14 @@ class RepresentCardEditActivity :
 
             bottomSheetDialog.show(supportFragmentManager, "sdsfs")
         }
+    }
+
+    private fun setTextGradient() {
+        val text = binding.tvRepresentcardeditColorTitle.text.toString()
+        val green = getColor(R.color.main_green)
+        val purple = getColor(R.color.main_purple)
+        val spannable = text.toSpannable()
+        spannable[0..text.length] = LinearGradientSpan(text, text, green, purple)
+        binding.tvRepresentcardeditColorTitle.text = spannable
     }
 }
