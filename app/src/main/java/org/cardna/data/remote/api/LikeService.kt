5937e128df5a5a38182d9c236302b07b591a16e8
username <ieejo716@naver.com>
2022-01-17 22:26:10 +0900

    #58 [FEAT] : Like Interface

--- app/src/main/java/org/cardna/data/remote/api/LikeService.kt
+++ app/src/main/java/org/cardna/data/remote/api/LikeService.kt
@@ -1,4 +1,16 @@
 package org.cardna.data.remote.api
 
+import org.cardna.data.remote.model.detail.RequestLikeData
+import org.cardna.data.remote.model.login.ResponseLikeData
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
 interface LikeService {
+    @Headers("Content-Type:application/json")
+    @POST("like")
+    fun postLike(
+        @Body body: RequestLikeData
+    ): Call<ResponseLikeData>
 }
