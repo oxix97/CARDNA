dcdc7b3391e74f8f3ab4b993cc9c026301110cd2
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-01-17 18:26:51 +0900

    [ADD] #56 - CardYou recyclerview go to item

--- app/src/main/java/org/cardna/ui/cardpack/CardYouFragment.kt
+++ app/src/main/java/org/cardna/ui/cardpack/CardYouFragment.kt
@@ -1,14 +1,19 @@
 package org.cardna.ui.cardpack
 
+import android.content.Intent
 import android.graphics.Rect
 import android.os.Bundle
 import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.cardna.R
 import org.cardna.base.baseutil.BaseViewUtil
+import org.cardna.data.remote.model.cardpack.ResponseCardPackMeData
 import org.cardna.data.remote.model.cardpack.ResponseCardPackYouData
 import org.cardna.databinding.FragmentCardYouBinding
+import org.cardna.ui.cardpack.adapter.CardPackMeRecyclerViewAdapter
 import org.cardna.ui.cardpack.adapter.CardPackYouRecyclerViewAdapter
+import org.cardna.ui.maincard.DetailActivity
 import org.cardna.util.SpacesItemDecoration
 import kotlin.math.roundToInt
 
@@ -28,17 +33,7 @@ class CardYouFragment : BaseViewUtil.BaseFragment<FragmentCardYouBinding>(org.ca
     }
 
     private fun initCardYouRvAdapter() {
-        cardYouAdapter = CardPackYouRecyclerViewAdapter()
-
-        binding.rvCardyou.adapter = cardYouAdapter
-
-        val gridLayoutManager = GridLayoutManager(requireContext(), 2)
-        binding.rvCardyou.layoutManager = gridLayoutManager
-
-        binding.rvCardyou.addItemDecoration(SpacesItemDecoration((12 * resources.displayMetrics.density).roundToInt()))
-
-
-        cardYouAdapter.cardList.addAll(
+        cardYouAdapter = CardPackYouRecyclerViewAdapter(
             listOf(
                 ResponseCardPackYouData("img url", 1, "댕댕이 짱 좋아"),
                 ResponseCardPackYouData("img url", 1, "댕댕이 짱 좋아"),
@@ -47,8 +42,22 @@ class CardYouFragment : BaseViewUtil.BaseFragment<FragmentCardYouBinding>(org.ca
                 ResponseCardPackYouData("img url", 1, "댕댕이 짱 좋아"),
                 ResponseCardPackYouData("img url", 1, "댕댕이 짱 좋아")
             )
-        )
+        ) {
+            val intent = Intent(requireContext(), DetailActivity::class.java).apply {
+                putExtra("cardImg", R.drawable.background_cardyou)
+                putExtra("id", 1)
+                putExtra("title", "테스트~~")
+                startActivity(this)
+            }
+        }
 
-        cardYouAdapter.notifyDataSetChanged()
+        with(binding) {
+            rvCardyou.adapter = cardYouAdapter
+
+            val gridLayoutManager = GridLayoutManager(requireContext(), 2)
+            rvCardyou.layoutManager = gridLayoutManager
+
+            rvCardyou.addItemDecoration(SpacesItemDecoration((12 * resources.displayMetrics.density).roundToInt()))
+        }
     }
 }
--- app/src/main/java/org/cardna/ui/cardpack/adapter/CardPackYouRecyclerViewAdapter.kt
+++ app/src/main/java/org/cardna/ui/cardpack/adapter/CardPackYouRecyclerViewAdapter.kt
@@ -4,29 +4,41 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import org.cardna.R
+import org.cardna.data.remote.model.cardpack.ResponseCardPackMeData
 import org.cardna.data.remote.model.cardpack.ResponseCardPackYouData
+import org.cardna.databinding.ItemCardpackCardmeBinding
 import org.cardna.databinding.ItemCardpackCardyouBinding
 
-class CardPackYouRecyclerViewAdapter : RecyclerView.Adapter<CardPackYouRecyclerViewAdapter.CardPackYouViewHolder>() {
 
-    val cardList = mutableListOf<ResponseCardPackYouData>()
+class CardPackYouRecyclerViewAdapter(
+    private val cardList: List<ResponseCardPackYouData>,
+    private val clickListener: (ResponseCardPackYouData) -> Unit
+) :
+    RecyclerView.Adapter<CardPackYouRecyclerViewAdapter.CardPackYouViewHolder>() {
 
-    inner class CardPackYouViewHolder(private val binding: ItemCardpackCardyouBinding) : RecyclerView.ViewHolder(binding.root) {
-        fun onBind(data: ResponseCardPackYouData ) {
-            binding.ivCardpackRecyclerview.setImageResource(R.drawable.dummy_img_cardpack_1)
-            binding.tvCardpackRecyclerview.text = data.title
+    inner class CardPackYouViewHolder(private val binding: ItemCardpackCardyouBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: ResponseCardPackYouData) {
+            with(binding) {
+                ivCardpackRecyclerview.setImageResource(R.drawable.dummy_img_cardpack_1)
+                tvCardpackRecyclerview.text = data.title
+                root.setOnClickListener {
+                    clickListener(data)
+                }
+            }
         }
     }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int
-    ): CardPackYouViewHolder {
-        val binding = ItemCardpackCardyouBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+    ): CardPackYouRecyclerViewAdapter.CardPackYouViewHolder {
+        val binding =
+            ItemCardpackCardyouBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return CardPackYouViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: CardPackYouViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: CardPackYouRecyclerViewAdapter.CardPackYouViewHolder, position: Int) {
         holder.onBind(cardList[position])
     }
 
