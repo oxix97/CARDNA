f09f8bf8a818fc9225d926eedd93f20900136564
minwoo <alsd20177@gmail.com>
2022-01-17 23:20:12 +0900

    [FEAT] #61 - add cardService API's data class

---
+++ app/src/main/java/org/cardna/data/remote/model/cardpack/ResponseCardMeData.kt
@@ -0,0 +1,23 @@
+package org.cardna.data.remote.model.cardpack
+
+
+data class ResponseCardMeData(
+    val status: Int,
+    val success: Boolean,
+    val message: String,
+    val data: Data
+){
+    data class Data(
+        val totalCardCnt: Int,
+        val isMyCard: Boolean,
+        val cardMeList: List<CardMe>
+    ){
+        data class CardMe(
+            val id: Int,
+            val cardImg: String,
+            val title: String,
+            val mainOrder: Int,
+            val isLiked: Boolean
+        )
+    }
+}
