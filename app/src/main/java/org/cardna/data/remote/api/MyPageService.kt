ca75701afd95f5fc72ceda9d129c8030c9babcf3
username <ieejo716@naver.com>
2022-01-17 22:26:05 +0900

    #58 [FEAT] : Mypage Interface

---
+++ app/src/main/java/org/cardna/data/remote/api/MyPageService.kt
@@ -0,0 +1,16 @@
+package org.cardna.data.remote.api
+
+import org.cardna.data.remote.model.mypage.ResponseMyPageData
+import org.cardna.data.remote.model.mypage.ResponseMyPageUserData
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+
+interface MyPageService {
+    @Headers("Content-Type:application/json")
+    @GET("mypage")
+    fun getMyPage(): Call<ResponseMyPageData>
+
+    @GET("my-page/user")
+    fun getMyPageUser(): Call<ResponseMyPageUserData>
+}
