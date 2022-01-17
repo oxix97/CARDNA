3dfdcae82f7550b936dda7f0a95acf90b8f599e9
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-01-17 21:57:58 +0900

    [ADD] #56 - Add NetworkService

---
+++ app/src/main/java/org/cardna/data/remote/api/AuthService.kt
@@ -0,0 +1,13 @@
+package org.cardna.data.remote.api
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface AuthService {
+
+
+
+}
