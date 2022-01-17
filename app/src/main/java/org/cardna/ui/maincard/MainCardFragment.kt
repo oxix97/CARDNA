81af84fe3e969d12895011f503b7b7d0f82b1cb6
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2022-01-17 16:52:30 +0900

    [FEAT] #45 - Maincard layout icon visibility setting

--- app/src/main/java/org/cardna/ui/maincard/MainCardFragment.kt
+++ app/src/main/java/org/cardna/ui/maincard/MainCardFragment.kt
@@ -3,7 +3,9 @@ package org.cardna.ui.maincard
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.core.text.set
+import androidx.core.text.toSpannable
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
@@ -12,9 +14,10 @@ import org.cardna.R
 import org.cardna.base.baseutil.BaseViewUtil
 import org.cardna.data.remote.model.maincard.MainCardListData
 import org.cardna.databinding.FragmentMainCardBinding
+import org.cardna.ui.cardpack.CardPackFragment
 import org.cardna.ui.maincard.adapter.MainCardAdapter
 import org.cardna.ui.representcardedit.RepresentCardEditActivity
-import kotlin.math.abs
+import org.cardna.util.LinearGradientSpan
 import kotlin.math.roundToInt
 
 class MainCardFragment :
@@ -27,9 +30,9 @@ class MainCardFragment :
 
     override fun initView() {
         initAdapter()
-        moveDetailActivity()
-        friendMainView()
-        moveAlarmActivity()
+        initClickEventCardYou()
+        setTextGradient()
+        initClickEventCardMe()
     }
 
     private fun initAdapter() {
@@ -113,24 +116,30 @@ class MainCardFragment :
     }
 
 
-    //상세 페이지로 이동
-    private fun moveDetailActivity() {
-        binding.llMaincardEditLayout.setOnClickListener {
-            val intent = Intent(requireActivity(), RepresentCardEditActivity::class.java)
-            startActivity(intent)
+    //내가 내 메인카드 볼떄 화면
+    private fun initClickEventCardMe() {
+        with(binding) {
+            //카드팩 보러가기 버튼 없애기
+            tvMaincardGotoCardpack.visibility = View.GONE
+            ivMaincardGotoCardpackBackground.visibility = View.GONE
+
+            //친구추가 버튼 없애기
+            ibtnMaincardFriend.visibility = View.GONE
+
+            //클릭 이벤트 달기
+            llMaincardEditLayout.setOnClickListener {
+                val intent = Intent(requireActivity(), RepresentCardEditActivity::class.java)
+                startActivity(intent)
+            }
+            ibtnMaincardAlarm.setOnClickListener {
+                val intent = Intent(requireActivity(), AlarmActivity::class.java)
+                startActivity(intent)
+            }
         }
     }
 
-    //알림 페이지로 이동
-    private fun moveAlarmActivity() {
-        binding.ibtnMaincardAlarm.setOnClickListener {
-            val intent = Intent(requireActivity(), AlarmActivity::class.java)
-            startActivity(intent)
-        }
-    }
-
-    //타인이 접근
-    private fun friendMainView() {
+    //타인이 내 메인카드 볼때 화면
+    private fun initClickEventCardYou() {
         var id: Int
         var name = ""
         if (getArguments() != null) {
@@ -139,5 +148,34 @@ class MainCardFragment :
         }
         //넘어온 user id에 따른 대표카드 list보여주기
         //fun requestFriendMainCard
+
+        with(binding) {
+            //카드팩 보러가기 버튼 보이기
+            tvMaincardGotoCardpack.visibility = View.VISIBLE
+            ivMaincardGotoCardpackBackground.visibility = View.VISIBLE
+
+
+            //친구추가 아이콘 보이기
+            ibtnMaincardFriend.visibility = View.VISIBLE
+
+            //카드 작성하기 아이콘 보이기
+            ibtnMaincardAlarm.setImageResource(R.drawable.ic_mypage_write)
+        }
+        //클릭 이벤트 달기
+        binding.tvMaincardGotoCardpack.setOnClickListener {
+            val transaction = requireActivity().supportFragmentManager.beginTransaction()
+                .addToBackStack(null)
+                .replace(org.cardna.R.id.fcv_main, CardPackFragment())
+            transaction.commit()
+        }
+    }
+
+    private fun setTextGradient() {
+        val text = binding.tvMaincardGotoCardpack.text.toString()
+        val green = requireActivity().getColor(R.color.main_green)
+        val purple = requireActivity().getColor(R.color.main_purple)
+        val spannable = text.toSpannable()
+        spannable[0..text.length] = LinearGradientSpan(text, text, green, purple)
+        binding.tvMaincardGotoCardpack.text = spannable
     }
 }
