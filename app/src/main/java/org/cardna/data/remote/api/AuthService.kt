a30e1427604b7d0b5059cd65b5bfe24916352a59
username <ieejo716@naver.com>
2022-01-17 22:24:56 +0900

    #58 [FEAT] :AuthService interface

--- app/src/main/java/org/cardna/data/remote/api/AuthService.kt
+++ app/src/main/java/org/cardna/data/remote/api/AuthService.kt
@@ -1,13 +1,29 @@
 package org.cardna.data.remote.api
 
+import org.cardna.data.remote.model.detail.ResponseRefreshTokenData
+import org.cardna.data.remote.model.login.RequestSignInEmailData
+import org.cardna.data.remote.model.login.RequestSignUpEmailData
+import org.cardna.data.remote.model.login.ResponseSignInEmailData
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface AuthService {
+    @Headers("Content-Type:application/json")
+    @POST("auth/signup/email")
+    fun postSignUp(
+        @Body body: RequestSignUpEmailData
+    ): Call<RequestSignUpEmailData>
 
+    @Headers("Content-Type:application/json")
+    @POST("auth/login/email")
+    fun postSignIn(
+        @Body body: RequestSignInEmailData
+    ): Call<ResponseSignInEmailData>
 
-
+    @GET("auth/refreshtoken")
+    fun getRefreshToken(): Call<ResponseRefreshTokenData>
 }
