3f1321e8e28700d19d9302edcf07615842a79cdd
username <ieejo716@naver.com>
2022-01-19 18:11:24 +0900

    [FIX] : /card/box coroutine error

--- app/src/main/java/org/cardna/ui/mypage/OtherWriteActivity.kt
+++ app/src/main/java/org/cardna/ui/mypage/OtherWriteActivity.kt
@@ -2,13 +2,14 @@ package org.cardna.ui.mypage
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.coroutines.launch
 import org.cardna.R
 import org.cardna.base.baseutil.BaseViewUtil
 import org.cardna.data.remote.api.ApiService
-import org.cardna.data.remote.model.mypage.ResponseCardYouBoxData
+import org.cardna.data.remote.model.mypage.ResponseCardYouBoxDataList
 import org.cardna.databinding.ActivityOtherWriteBinding
 import org.cardna.ui.maincard.DetailActivity
 import org.cardna.ui.mypage.adapter.OtherWriteRecyclerViewAdapter
@@ -16,6 +17,7 @@ import org.cardna.ui.mypage.adapter.OtherWriteRecyclerViewAdapter
 class OtherWriteActivity :
     BaseViewUtil.BaseAppCompatActivity<ActivityOtherWriteBinding>(R.layout.activity_other_write) {
 
+    private lateinit var dataContainer:MutableList<ResponseCardYouBoxDataList>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         initView()
@@ -23,25 +25,12 @@ class OtherWriteActivity :
 
     override fun initView() {
         initCoroutine()
+        Log.d("하아",dataContainer.size.toString())
+        initAdapter()
     }
-
-    private fun initAdapter(data: List<ResponseCardYouBoxData>) {
-        // val cardList = mutableListOf(
-        //     ResponseOtherWriterData(1, "종잔", "칭구", "가빈", "2022/42/42", true),
-        //     ResponseOtherWriterData(1, "바보", "칭구", "나빈", "2022/42/42", true),
-        //     ResponseOtherWriterData(1, "민우", "칭구", "다빈", "2022/42/42", true),
-        //     ResponseOtherWriterData(1, "바보", "칭구", "라빈", "2022/42/42", true),
-        //     ResponseOtherWriterData(1, "다빈", "칭구", "마빈", "2022/42/42", true),
-        //     ResponseOtherWriterData(1, "천제", "칭구", "바빈", "2022/42/42", true),
-        //     ResponseOtherWriterData(1, "ㅎㅇ", "칭구", "사빈", "2022/42/42", true),
-        //     ResponseOtherWriterData(1, "ㅎㅇ", "칭구", "아빈", "2022/42/42", true),
-        //     ResponseOtherWriterData(1, "ㅎㅇ", "칭구", "자빈", "2022/42/42", true),
-        //     ResponseOtherWriterData(1, "ㅎㅇ", "칭구", "차빈", "2022/42/42", true),
-        //     ResponseOtherWriterData(1, "ㅎㅇ", "칭구", "카빈", "2022/42/42", true),
-        //     ResponseOtherWriterData(1, "ㅎㅇ", "칭구", "카빈", "2022/42/42", false),
-        // )
-        val cardList = mutableListOf<ResponseCardYouBoxData.Data>()
-        val adpater = OtherWriteRecyclerViewAdapter(cardList) { position ->
+  //  data: MutableList<ResponseCardYouBoxDataList>
+    private fun initAdapter() {
+        val adapter = OtherWriteRecyclerViewAdapter(dataContainer) { position ->
             Intent(this, DetailActivity::class.java).apply {
                 putExtra("name", position.name)
                 putExtra("image", position.relation)
@@ -50,21 +39,25 @@ class OtherWriteActivity :
             }
         }
         with(binding) {
-            rvOtherwriteList.adapter = adpater
+            rvOtherwriteList.adapter = adapter
             binding.rvOtherwriteList.layoutManager = LinearLayoutManager(this@OtherWriteActivity)
-            adpater.submitList(cardList) // 아이템 업데이트
+            adapter.notifyDataSetChanged()
+       //     adapter.submitList(cardList) // 아이템 업데이트
         }
     }
 
     private fun initCoroutine() {
         lifecycleScope.launch {
             try {
-                val dataContainer = ApiService.cardService.getCardBox()
-                val cardList = mutableListOf<ResponseCardYouBoxData>()
-                initAdapter(cardList)
-            } catch (e: Exception) {
+                dataContainer = ApiService.cardService.getCardBox().responseCardYouBoxDataList
 
+            } catch (e: Exception) {
+                e.printStackTrace()
             }
         }
+
     }
+
+
+
 }
--- app/src/main/java/org/cardna/ui/mypage/adapter/OtherWriteRecyclerViewAdapter.kt
+++ app/src/main/java/org/cardna/ui/mypage/adapter/OtherWriteRecyclerViewAdapter.kt
@@ -6,20 +6,19 @@ import androidx.core.view.isVisible
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import org.cardna.data.remote.model.mypage.ResponseCardYouBoxData
+import org.cardna.data.remote.model.mypage.ResponseCardYouBoxDataList
 import org.cardna.databinding.ItemOtherwriteBinding
 
 class OtherWriteRecyclerViewAdapter(
-    private val cardList: List<ResponseCardYouBoxData.Data>,
-    private val clickListener: (ResponseCardYouBoxData.Data) -> Unit
-) : ListAdapter<ResponseCardYouBoxData.Data, OtherWriteRecyclerViewAdapter.OtherWriterViewHolder>(
+    private val cardList: MutableList<ResponseCardYouBoxDataList>,
+    private val clickListener: (ResponseCardYouBoxDataList) -> Unit
+) : ListAdapter<ResponseCardYouBoxDataList, OtherWriteRecyclerViewAdapter.OtherWriterViewHolder>(
     diffUtil
 ) {
 
     inner class OtherWriterViewHolder(private val binding: ItemOtherwriteBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        fun onBind(data: ResponseCardYouBoxData.Data) {
+        fun onBind(data: ResponseCardYouBoxDataList) {
             binding.apply {
                 tvOtherwriteTitle.text = data.title
                 tvOtherwriteRelation.text = data.relation
@@ -48,16 +47,16 @@ class OtherWriteRecyclerViewAdapter(
     override fun getItemCount(): Int = cardList.size
 
     companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<ResponseCardYouBoxData.Data>() {
+        val diffUtil = object : DiffUtil.ItemCallback<ResponseCardYouBoxDataList>() {
             override fun areContentsTheSame(
-                oldItem: ResponseCardYouBoxData.Data,
-                newItem: ResponseCardYouBoxData.Data
+                oldItem: ResponseCardYouBoxDataList,
+                newItem: ResponseCardYouBoxDataList,
             ) =
                 oldItem == newItem
 
             override fun areItemsTheSame(
-                oldItem: ResponseCardYouBoxData.Data,
-                newItem: ResponseCardYouBoxData.Data
+                oldItem:ResponseCardYouBoxDataList,
+                newItem:ResponseCardYouBoxDataList,
             ) =
                 oldItem.id == newItem.id
         }
