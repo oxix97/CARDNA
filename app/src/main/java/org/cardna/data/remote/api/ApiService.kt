c37ac4d6aa84b9d0b7a6417d9b4e40269a37e123
minwoo <alsd20177@gmail.com>
2022-01-21 15:42:44 +0900

    [MOD] #90 - modify many xml

--- app/src/main/java/org/cardna/data/remote/api/ApiService.kt
+++ app/src/main/java/org/cardna/data/remote/api/ApiService.kt
@@ -40,7 +40,7 @@ class AppInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain)
         : okhttp3.Response = with(chain) {
         val newRequest = request().newBuilder()
-            .addHeader("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MywiZW1haWwiOiJqaXdvb0BnbWFpbC5jb20iLCJuYW1lIjoi7KeA7JqwIiwiZmlyZWJhc2VJZCI6ImVpSk9qSHJMTWtVOEpiRTg4TUQxWGp6UWhYRzIiLCJpYXQiOjE2NDI2NjA2MjAsImV4cCI6MTY0NTI1MjYyMCwiaXNzIjoiY2FyZG5hIn0.ZW9Ot20SauGijUFjKv5BDb8Ix48RFONvk0Jn_Z0MQYg")
+            .addHeader("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZW1haWwiOiJqdW5namFlQGdtYWlsLmNvbSIsIm5hbWUiOiLsoJXsnqwiLCJmaXJlYmFzZUlkIjoidnZJMXFERTVsS2F3NTEyYkl5bUNIekN2SEx1MiIsImlhdCI6MTY0MjQ4NDQyNCwiZXhwIjoxNjQ1MDc2NDI0LCJpc3MiOiJjYXJkbmEifQ.yNk3jLxi6t37oq1p8ShwEghCyGzlv9s-lT3Y7VN-SZg")
             .build()
         proceed(newRequest)
     }
