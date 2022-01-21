cffd03128c276a96f1ec86c8b7c0bfdb0ef13d18
minwoo <alsd20177@gmail.com>
2022-01-21 18:10:45 +0900

    [FIX] #90 - modify MyPageFragment recyclerView padding

--- app/src/main/java/org/cardna/ui/representcardedit/RepresentCardEditActivity.kt
+++ app/src/main/java/org/cardna/ui/representcardedit/RepresentCardEditActivity.kt
@@ -133,7 +133,7 @@ class RepresentCardEditActivity :
     }
 
 
-    private fun initCoroutine() {
+    fun initCoroutine() {
         lifecycleScope.launch {
             try {
                 val dataContainer = ApiService.cardService.getUserMainCard()
